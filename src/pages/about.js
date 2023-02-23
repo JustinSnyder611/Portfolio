697187d2cd30a4ad8ea151bb914824842a5c1e96
JustinSnyder611 <JustinSnyder611@gmail.com>
2023-02-23 17:24:21 -0600

    fixed about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,8 @@
 const About = () => {
     return (
         <div>
-            <h2>About Me</h2>
-            <p id='aboutText'>I am a 20 year old Texas resident who is originally from Colorado.</p>
+            <h2>About Justin Snyder</h2>
+            <p id='aboutText'>I am a 20 year old web developer fresh out of a Univeristy of Texas bootcamp. During this bootcamp we learned everything that prepared me for the job. From HTML all the way to React, and even computer science.</p>
         </div>
     );
 };
