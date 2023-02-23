36b01db25b79e53cf32ded01d2814eacfece3ddf
JustinSnyder611 <JustinSnyder611@gmail.com>
2023-02-23 17:11:16 -0600

    added homepage

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,7 +1,10 @@
+import welcomeImg from '../components/pic1.png';
+
 const Homepage = () => {
     return (
         <div>
-            <h2>Homepage</h2>
+            <h2 id="welcomeText">Welcome to my Portfolio!</h2>
+            <img id='cssPic' src={welcomeImg} alt='css code'></img>
         </div>
     );
 };
